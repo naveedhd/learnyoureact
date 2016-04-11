6e22c50084f153cc76fad78f273ffc35b1ea3c73
Naveed Usmani <naveedhd@gmail.com>
2016-04-11 21:51:22 +0900

    [not working] Challenge 8: ISOMORPH

--- program.js
+++ program.js
@@ -1,3 +1,13 @@
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM =  React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+var babelify = require("babelify");
+
 var express = require('express');
 var app = express();
 
@@ -14,6 +24,8 @@ require('babel/register')({
   ignore: false
 });
 
+var TodoBox = require('./views/index.jsx');
+
 // write below
 var data = [
   {
@@ -26,8 +38,36 @@ var data = [
   }
 ];
 
+app.use('/bundle.js', function (req, res) {
+  res.setHeader('content-type', 'application/javascript');
+
+  browserify({ debug: true })
+    .transform(babelify.configure({
+      presets: ["react", "es2015"],
+      compact: true
+    }))
+    .require("./app.js", { entry: true })
+    .bundle()
+    .pipe(res);
+});
+
 app.use('/', function(req, res) {
-  res.render('index', {data: data});
+  var intialData = JSON.stringify(data);
+  var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+  res.setHeader('Content-Type', 'text/html');
+
+  var html = ReactDOMServer.renderToStaticMarkup(body(null,
+    div({id: 'app', dangerouslySetInnetHTML: {__html:markup}}),
+    script({
+      id: 'initial-data',
+      type: 'text/plain',
+      'data-json': intialData
+    }),
+    script({src: '/bundle.js'})
+  ));
+
+  res.end(html);
 });
 
 app.listen(app.get('port'), function() {});
